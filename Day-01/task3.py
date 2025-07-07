d90370180b93b244580dad5194d30c590c7da3e2
Geethika100275 <geethikaragesh@gmail.com>
2025-07-07 23:10:12 +0530

    Create task3.py

---
+++ Day-01/task3.py
@@ -0,0 +1,4 @@
+name=input("What's your name?")
+age=input("What's your age?")
+hobby=input("What's your hobby?")
+print(f"Hii! this is {name}. I am {age} years old. I love {hobby}.Nice to meet you!!!")
