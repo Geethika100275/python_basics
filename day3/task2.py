c094a742d6978793a67636d0f29d6b84f8502065
Geethika100275 <geethikaragesh@gmail.com>
2025-07-09 18:10:47 +0530

    Create task2.py

---
+++ day3/task2.py
@@ -0,0 +1,7 @@
+coding=input("Do you like coding?")
+if (coding)=="yes":
+    print("Awsome!!!!!!!")
+elif (coding)=="no":
+    print("me too!!")
+else:
+    print("Why are you acting like a fool???")
