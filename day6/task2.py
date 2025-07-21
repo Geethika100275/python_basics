e535e9012c4800345dd1fe2ec7a38289403ffa4e
Geethika100275 <geethikaragesh@gmail.com>
2025-07-22 03:12:39 +0530

    Create day6/task2.py

---
+++ day6/task2.py
@@ -0,0 +1,7 @@
+
+fruit_counts={'apple': 5, 'banana': 3}
+print("Original dictionary:", fruit_counts
+fruit_counts['orange']=8
+print("After adding orange:", fruit_counts)
+fruit_counts['apple']=10
+print("After updating apple:", fruit_counts)
