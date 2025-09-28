de162732a088ae7c4263bc2c6c77f62519553e53
Geethika100275 <geethikaragesh@gmail.com>
2025-09-28 22:09:26 +0530

    Create dict1.py

---
+++ dict1.py
@@ -0,0 +1,11 @@
+def dictionary(students):
+     students = ["Alice", "Bob", "alice", "ALICE", "bob", "Charlie"]
+     dic={}
+     for student in students:
+         
+         
+         dic=dic["Alice"]
+         
+     return dic
+students = ["Alice", "Bob", "alice", "ALICE", "bob", "Charlie"]     
+print(dictionary(students))
